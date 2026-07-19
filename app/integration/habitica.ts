93921494dc1c7ba462cb474ff41d91f8604346fd
Neemias <neemiassgc@outlook.com>
2026-07-19 16:33:57 -0300

    refactor(integration/habitica): extract headers into separate function

--- app/integration/habitica.ts
+++ app/integration/habitica.ts
@@ -1,14 +1,18 @@
 export async function getCoins(): Promise<string> {
   const res = await fetch("https://habitica.com/api/v3/user/anonymized", {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": process.env["API_TOKEN"] as string,
-      "x-api-user": process.env["USER_ID"] as string,
-      "x-client": process.env["USER_ID"]+"-coinclaimer"
-    },
+    headers: headers(),
   });
   const response = await res.json();
   const coins = response.data.user.stats.gp
   return coins.toFixed(2);
+}
+
+function headers() {
+  return {
+    "Content-Type": "application/json",
+    "x-api-key": process.env["API_TOKEN"] as string,
+    "x-api-user": process.env["USER_ID"] as string,
+    "x-client": process.env["USER_ID"]+"-coinclaimer"
+  }
 }
