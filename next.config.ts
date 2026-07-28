29efeb3616e3d91898b739cec89e477d3bbc01af
Neemias <neemiassgc@outlook.com>
2026-07-28 11:36:38 -0300

    feat(next.config): add allowedDevOrigins for ngrok dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['pro-firm-bear.ngrok-free.app'],
 };
 
 export default nextConfig;
