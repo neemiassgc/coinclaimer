4f13cdc0ebb0fceb550acd2745dad4323b91bdbf
Neemias <neemiassgc@outlook.com>
2026-07-28 11:26:07 -0300

    refactor: remove revalidate route

--- app/action/revalidate/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
-
-export async function POST(request: Request) {
-  revalidatePath("/")
-  return new NextResponse("ok", {
-    status: 200,
-  });
-}
