701f1c6798ff5a69b051df863cfdfe3f38cfe2af
Neemias <neemiassgc@outlook.com>
2026-08-16 03:47:30 -0300

    feat(scan): add route to adjust Habitica coins based on task completion

---
+++ app/action/scan/route.ts
@@ -0,0 +1,51 @@
+import { getCoins, getTaskDetail, getTaskGroup, setCoins } from "@/app/habitica";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await scan();
+  return new NextResponse("Ok", { status: 200 });
+}
+
+async function scan(): Promise<void> {
+  const taskGroup = await getTaskGroup();
+  const habitTaskDetail = await getTaskDetail(taskGroup.habit[0]);
+  if (habitTaskDetail.data.counterUp === 0) {
+    await setCoins("0");
+    return;
+  }
+
+  for (const taskId of taskGroup.daily) {
+    const dailyTaskDetail = await getTaskDetail(taskId);
+    if (!dailyTaskDetail.data.completed) {
+      await cutInHalf();
+      return;
+    }
+  }
+
+
+  for (const taskId of taskGroup.todo) {
+    const todoTaskDetail = await getTaskDetail(taskId);
+    if (isToday(todoTaskDetail.data.date)) {
+      await cutInHalf();
+      return;
+    }
+  }
+}
+
+function isToday(isoUtc: string): boolean {
+  const timezoneId = "America/Sao_Paulo";
+  
+  const target = Temporal.Instant.from(isoUtc)
+    .toZonedDateTimeISO(timezoneId)
+    .toPlainDate();
+
+  const today = Temporal.Now.plainDateISO(timezoneId);
+
+  return target.equals(today);
+}
+
+async function cutInHalf(): Promise<void> {
+  const currentAmountOfCoins = await getCoins();
+  const updatedAmountOfCoins = Math.round(currentAmountOfCoins / 2);
+  await setCoins(updatedAmountOfCoins+"");
+}
